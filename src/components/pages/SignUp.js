01df12f334a793068e3ffe54685b5958de6f9863
Uzeyir <uzeyir.bagirov@gmail.com>
2021-08-23 16:20:22 -0400

    login/signup

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,72 @@
 import React from 'react';
-import '../../App.css';
+import '../login/style.scss'
+import {Register, Login} from "../login/index";
+import './SignUp.scss';
 
-export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true
+    }
+  }
+   
+  componentDidMount() {
+    //Add .right by default
+    this.rightSide.classList.add("right");
+  }
+
+  changeState() {
+    const { isLogginActive } = this.state;
+
+    if (isLogginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    } else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
+    }
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
+  }
+
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Register" : "Login";
+    const currentActive = isLogginActive ? "login" : "register";
+    return (
+    <div className='sign-up'>
+      <div className="login">
+        <div className="container">
+        {isLogginActive &&  (<Login containerRef={ref => (this.current = ref)}/>)}
+        {!isLogginActive && (<Register containerRef={ref => (this.current = ref)}/>)}
+        </div>
+        <RightSide
+            current={current}
+            currentActive={currentActive}
+            containerRef={ref => (this.rightSide = ref)}
+            onClick={this.changeState.bind(this)}
+          />
+      </div>
+    </div>
+    )
+  }
 }
+
+const RightSide = props => {
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
+    </div>
+  );
+};
+
+export default  SignUp;
